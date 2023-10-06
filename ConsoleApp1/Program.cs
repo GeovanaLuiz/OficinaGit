3d3c7da5f80e593f4a00154a239f4c26251dc7f6
Geovana <geovanals1206@gmail.com>
2023-10-06 09:39:03 -0400

    segundo commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class program
+{
+    static void Main()
+    {
+
+        Console.Write("\nDigite um número: ");
+        double num = Convert.ToInt32 (Console.ReadLine());
+        Console.Write("\nDigite outro número: ");
+        double num2 = Convert.ToInt32(Console.ReadLine());
+
+        double resultado = num + num2;
+
+        if (resultado == 0){ Console.WriteLine("Numero Inválido"); }
+        if (resultado == 1) { Console.WriteLine("Numero Inválido"); }
+        if (resultado == 2) { Console.WriteLine("Numero Inválido"); }
+
+        Console.WriteLine($"O resultado da multiplicação é {resultado}");
+
+        Console.ReadKey();
+    }
+}
